4aa1658f39fc897aa185219bd8af2ddb3d3a3782
Dylan Landry <dylanglandry@gmail.com>
2019-03-25 13:35:56 -0400

    Better mobile readability, reduce post width.

--- src/scripts/set-post-content-width.js
+++ src/scripts/set-post-content-width.js
@@ -2,7 +2,7 @@ module.exports = function () {
   const tenChars = document.querySelector(`.set-content-width span`)
   const tenCharsWidth = tenChars.offsetWidth
   tenChars.style.display = `none`
-  const eightyCharsWidth = tenCharsWidth * 8
+  const sixtyCharsWidth = tenCharsWidth * 6
   const postContent = document.querySelector(`.post-content`)
-  postContent.style.maxWidth = eightyCharsWidth
+  postContent.style.maxWidth = sixtyCharsWidth
 }
